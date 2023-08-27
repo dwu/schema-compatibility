c6079c231736999040f877bb64127c2a55e12ebc
Daniel Wutke <dwu.public@gmail.com>
2023-08-27 15:16:33 +0200

    Add -t / --test mode
    Checks BACKWARD, FORWARD, FULL compatibility for two given files.

--- README.md
+++ README.md
@@ -62,14 +62,20 @@ Command line arguments:
 
 ```
 usage: schema-compatibility
- -d,--directory <arg>   Directory containing schema compatibility test
-                        case description files (scanned recursively)
- -f,--file <arg>        Schema compatibilty test case description file
- -h,--help              Show help
- -j,--json              Show result in JSON format for further processing
-```
+ -d,--directory <arg>    Directory containing schema compatibility test
+                         case description files (scanned recursively)
+ -f,--file <arg>         Schema compatibilty test case description file
+ -h,--help               Show help
+ -j,--json               Show result in JSON format for further processing
+ -n,--newschema <arg>    New schema file (used only when in 'test' mode)
+ -o,--oldschema <arg>    Old schema file (used only when in 'test' mode)
+ -s,--schematype <arg>   Type of the schema, i.e. 'AVRO', 'PROTOBUF',
+                         'JSON' (used only when in 'test' mode)
+ -t,--test               Run in test mode; perform all possible
+                         compatibility checks (requires -o and -n)
+ ```
 
-Usage example (plaintext output):
+Usage example, case mode (plaintext output):
 
 ```shell
 $ java -jar target/schema-compatibility-1.0-SNAPSHOT-jar-with-dependencies.jar -f src/test/resources/avro-not-backward-compatible.yml
@@ -108,10 +114,21 @@ New schema:
 }
 ```
 
-Usage example (JSON output):
+Usage example, case mode (JSON output):
 
 ```shell
 $ java -jar target/schema-compatibility-1.0-SNAPSHOT-jar-with-dependencies.jar -j -d ./src/test/resources
 {"success":true,"expected":"NOT_COMPATIBLE","got":"NOT_COMPATIBLE","compatibility":"BACKWARD","oldschema":"{\"namespace\": \"example.avro\",\n \"type\": \"record\",\n \"name\": \"User\",\n \"fields\": [\n     {\"name\": \"name\", \"type\": \"string\"},\n     {\"name\": \"favorite_number\",  \"type\": [\"long\", \"null\"]},\n     {\"name\": \"favorite_color\", \"type\": [\"string\", \"null\"]}\n ]\n}\n","newschema":"{\"namespace\": \"example.avro\",\n \"type\": \"record\",\n \"name\": \"User\",\n \"fields\": [\n     {\"name\": \"name\", \"type\": \"string\"},\n     {\"name\": \"favorite_number\",  \"type\": [\"int\", \"null\"]},\n     {\"name\": \"favorite_color\", \"type\": [\"string\", \"null\"]}\n ]\n}","messages":["{errorType:\u0027MISSING_UNION_BRANCH\u0027, description:\u0027The new schema is missing a type inside a union field at path \u0027/fields/1/type/0\u0027 in the old schema\u0027, additionalInfo:\u0027reader union lacking writer type: LONG\u0027}"]}
 {"success":true,"expected":"NOT_COMPATIBLE","got":"NOT_COMPATIBLE","compatibility":"BACKWARD","oldschema":"syntax \u003d \"proto3\";\nmessage SearchRequest {\n  string query \u003d 1;\n  int32 page_number \u003d 2;\n  int32 results_per_page \u003d 3;\n}\n","newschema":"syntax \u003d \"proto3\";\nmessage SearchRequest {\n  string query \u003d 1;\n  float page_number \u003d 2;\n  int32 results_per_page \u003d 3;\n}","messages":["{errorType:\"FIELD_SCALAR_KIND_CHANGED\", description:\"The kind of a SCALAR field at path \u0027#/SearchRequest/2\u0027 in the new schema does not match its kind in the old schema\"}"]}
+```
+
+Usage example, test mode (checks BACKWARD, FORWARD, FULL for two given schema files):
+
+```shell
+$ java -jar target/schema-compatibility-1.0-SNAPSHOT-jar-with-dependencies.jar -t -s AVRO -o user-v1.avsc -n user-v2.avsc
+BACKWARD: FAIL
+  - {errorType:'READER_FIELD_MISSING_DEFAULT_VALUE', description:'The field 'lastname' at path '/fields/1' in the new schema has no default value and is missing in the old schema', additionalInfo:'lastname'}
+FORWARD: OK
+FULL: FAIL
+  - {errorType:'READER_FIELD_MISSING_DEFAULT_VALUE', description:'The field 'lastname' at path '/fields/1' in the new schema has no default value and is missing in the old schema', additionalInfo:'lastname'}
 ```
--- src/main/java/io/github/dwu/schemacompatibility/App.java
+++ src/main/java/io/github/dwu/schemacompatibility/App.java
@@ -4,20 +4,21 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import io.confluent.kafka.schemaregistry.CompatibilityLevel;
 import io.confluent.kafka.schemaregistry.exceptions.InvalidSchemaException;
 import io.github.dwu.schemacompatibility.desc.Case;
 import org.apache.commons.cli.*;
+import org.apache.commons.io.FileUtils;
 import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class App {
@@ -62,6 +63,23 @@ public class App {
         }
     }
 
+    public void checkCompatibility(String schemaType, String oldschemaFilename, String newschemaFilename) throws InvalidSchemaException, IOException {
+        String oldschema = FileUtils.readFileToString(new File(oldschemaFilename), "UTF-8");
+        String newschema  = FileUtils.readFileToString(new File(newschemaFilename), "UTF-8");
+        List<CompatibilityLevel> levels = Arrays.asList(CompatibilityLevel.BACKWARD, CompatibilityLevel.FORWARD, CompatibilityLevel.FULL);
+        for (CompatibilityLevel level : levels) {
+            List<String> compatibilityCheckResult = compatibilityChecker.checkSchemaCompatibility(schemaType, level, oldschema, newschema);
+            compatibilityCheckResult.removeIf(s -> s.startsWith("{oldSchema: "));
+
+            String success = compatibilityCheckResult.size() == 0 ? "OK" : "FAIL";
+            System.out.println(level.toString() + ": " + success);
+
+            for (String message : compatibilityCheckResult) {
+                System.out.println("  - " + message);
+            }
+        }
+    }
+
     public void setResultFormatter(ResultFormatter resultFormatter) {
         this.resultFormatter = resultFormatter;
     }
@@ -71,6 +89,10 @@ public class App {
         options.addOption("j", "json", false, "Show result in JSON format for further processing");
         options.addOption("f", "file", true, "Schema compatibilty test case description file");
         options.addOption("d", "directory", true, "Directory containing schema compatibility test case description files (scanned recursively)");
+        options.addOption("t", "test", false, "Run in test mode; perform all possible compatibility checks (requires -o and -n)");
+        options.addOption("o", "oldschema", true, "Old schema file (used only when in 'test' mode)");
+        options.addOption("n", "newschema", true, "New schema file (used only when in 'test' mode)");
+        options.addOption("s", "schematype", true, "Type of the schema, i.e. 'AVRO', 'PROTOBUF', 'JSON' (used only when in 'test' mode)");
         options.addOption("h", "help", false, "Show help");
 
         CommandLineParser parser = new DefaultParser();
@@ -93,10 +115,16 @@ public class App {
             app.checkCaseFile(cli.getOptionValue("f"));
         } else if (cli.hasOption("d")) {
             app.checkCaseDirectory(cli.getOptionValue("d"));
+        } else if (cli.hasOption("t")) {
+            if (!cli.hasOption("o") || !cli.hasOption("n") || !cli.hasOption("s")) {
+                System.err.println("ERROR: Test mode requires 'schematype', 'oldschema' and 'newschema' parameters");
+                printHelpAndExit(options);
+            }
+            app.checkCompatibility(cli.getOptionValue("s"), cli.getOptionValue("o"), cli.getOptionValue("n"));
         } else if (cli.hasOption("h")) {
             printHelpAndExit(options);
         } else {
-            System.err.println("ERROR: One of 'file' and 'path' arguments must be specified");
+            System.err.println("ERROR: One of 'test', 'file' or 'path' arguments must be specified");
             printHelpAndExit(options);
         }
     }
